7d35f0fc1d819e1c55010704cb168ccfeb90e8e9
wahid shaikh <wahidm@avalonconsult.com>
2019-09-07 11:39:03 +0530

    added validation to Project Object

--- ppmtool/src/main/java/com/nseit/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/com/nseit/ppmtool/domain/Project.java
@@ -1,11 +1,16 @@
 package com.nseit.ppmtool.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.util.Date;
 
@@ -14,13 +19,21 @@ public class Project {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	@NotBlank(message = "Project name is required")
 	private String projectName;
+	@NotBlank(message = "Project identifier is required")
+	@Size(min=4, max=5, message = "Please use 4 to 5 characters") //ex.. WMSPRTL
+	@Column(updatable = false, unique = true)
 	private String projectIdentifier;
+	@NotBlank(message = "Project description is required")
 	private String description;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date start_date;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date end_date;
-	
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date created_at;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date updated_at;
 	
 	public Project(){
