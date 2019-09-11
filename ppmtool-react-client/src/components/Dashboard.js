d3206b14f53dd9adf4152bf1278a12e7a15e2fbe
wahid shaikh <wahidm@avalonconsult.com>
2019-09-11 18:06:19 +0530

    creatd header and project item sample for our dashboard page

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
 
 class Dashboard extends Component {
   render() {
-    return <h1>Welcome to the PPM tool development......</h1>;
+    return (
+      <div>
+        <h1>Welcome to the PPM tool development......</h1>
+        <ProjectItem></ProjectItem>
+      </div>
+    );
   }
 }
 
