898aacf21e5784f0f4259c2541e30a394752d362
wahid shaikh <wahidm@avalonconsult.com>
2019-09-13 08:02:54 +0530

    setting up redux store for a PPM tool project

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -5,17 +5,21 @@ import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header></Header>
-          <Route exact path="/dashboard" component={Dashboard}></Route>
-          <Route exact path="/addProject" component={AddProject}></Route>
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header></Header>
+            <Route exact path="/dashboard" component={Dashboard}></Route>
+            <Route exact path="/addProject" component={AddProject}></Route>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
